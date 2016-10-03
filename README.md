884f86bdbe900554d080663f4dbd0eb9707850e2
Bjorngulf <bjorn.vantilt@gmail.com>
2016-10-03 10:17:57 +0200

    Now we don't need root priv anymore :)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Masterproef
 Master thesis using LWIR camera with a normal camera to improve human detection in bad weather conditions.
+
+
+udev file so we don't need root permissions
+	SUBSYSTEMS=="usb", ATTRS{idVendor}=="289d", ATTRS{idProduct}=="0010", MODE="0660", GROUP="input"
+Don't forget to add your user to the right group
--- seek_thermal/seek.cpp
+++ seek_thermal/seek.cpp
@@ -59,14 +59,6 @@ void caminterface::init()
 			throw runtime_error("Failed to get device descriptor");
 		}
 
-
-		res = libusb_open(dev, &handle);
-		if (res < 0) {
-			libusb_free_device_list(devs, 1);
-			throw runtime_error("Failed to open device");
-			continue;
-		}
-
 		if (desc.idVendor == 0x289d && desc.idProduct == 0x0010) {
 			found = true;
 			break;
@@ -76,6 +68,12 @@ void caminterface::init()
 	if (!found) {
 		libusb_free_device_list(devs, 1);
 		throw runtime_error("Seek not found");
+	}
+	
+	res = libusb_open(dev, &handle);
+	if (res < 0) {
+		libusb_free_device_list(devs, 1);
+		throw runtime_error("Failed to open device");
 	}
 
 	bugprintf("Device found\n");

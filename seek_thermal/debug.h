29f441d78d095dc7840a1a708e6840f5d1a335b7
Bjorngulf <bjorn.vantilt@gmail.com>
2016-12-11 15:20:14 +0100

    New bugout

--- seek_thermal/debug.h
+++ seek_thermal/debug.h
@@ -1,10 +1,12 @@
 #ifndef __DEBUG_H_
 #define __DEBUG_H_
 
-#define DEBUG	0
+#define DEBUG 0
+
+#define bugline cout << __FILE__ << " " << __LINE__ << " " << __func__ << endl;
 
 #if defined(DEBUG) && DEBUG > 0
- #define bugprintf(fmt, args...) fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt, \
+ #define bugprintf(fmt, args...) fprintf(stdout, "DEBUG: %s:%d:%s(): " fmt, \
     __FILE__, __LINE__, __func__, ##args)
 #else
  #define bugprintf(fmt, args...) /* Don't do anything in release builds */

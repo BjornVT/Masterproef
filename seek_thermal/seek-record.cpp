3642ba72ec0e49a18fac73b7894a5f188a6f8681
Bjorngulf <bjorn.vantilt@gmail.com>
2016-12-11 15:39:22 +0100

    All new recording program.

--- seek_thermal/seek-record.cpp
+++ seek_thermal/seek-record.cpp
@@ -6,117 +6,137 @@
 #include <cstdint>
 #include <chrono>
 #include <signal.h>
+#include <thread>
+#include <mutex>
+#include <unistd.h>
 #include <opencv2/highgui/highgui.hpp>
 
 #include "seek.hpp"
 #include "debug.h"
+#include "camSettings.hpp"
 
-#define FPS 		8
-#define NAMELWIR 	"outlwir.avi"
-#define NAMERGB		"outrgb.avi"
+#define RGB 0
+#define LWIR 1
 
 using namespace std;
 using namespace cv;
 
-bool running = true;
-
-void quitProgram(int sig);
+/* Functions */
 static void help(char* name);
+void task(VideoCapture *cap);
 
+/* Global vars for threading */
+bool running = true;
+Mat buffer;
+mutex mtxCam;
+
+/* Main */
 int main(int argc, char** argv) {
 	LibSeek::seekCam seek;
-	Mat frameLWIR;
-	Mat frameRGB;
 	VideoCapture cap;
-	VideoWriter outLWIR;
-	VideoWriter outRGB;
+	Mat frame[2];
+	thread t;
 	int nimg = 0;
 	
-	signal(SIGTERM, &quitProgram);
-	signal(SIGINT, &quitProgram);
-	
-	if(argc > 3 || argc == 1){
+	/* Is there a smart person on the other side? */
+	if(argc != 3){
 		throw runtime_error("Check args");
 		help(argv[0]);
 		exit(1);
 	}
-	else if (argc == 2){
-		cap.open(0);
-	}
-	else {
-		cap.open(argv[2]);
-	}
+
+	/* Hi there camera's. Get to work */
+	cap.open(stoi(argv[1]));
+	seek.open(argv[2]);
 	
-	// check of webcam device number is correct
-	if(!cap.isOpened())
+	/* Everyone onboard? */
+	if(!(cap.isOpened() && seek.isOpened()))
 	{
-		throw runtime_error("Failed to open RGB camera");
+		throw runtime_error("Failed to open camera");
 		exit(1);
 	}
-	cap.set(CV_CAP_PROP_FPS, 0); //Hack so the VideoCapture buffer doesn't fill
 	
-	
-	Size SRGB = Size((int) cap.get(CV_CAP_PROP_FRAME_WIDTH),    // Acquire input size
-					(int) cap.get(CV_CAP_PROP_FRAME_HEIGHT));
-	Size SLWIR = Size((int)208, (int)156);
-	
-	//Opening video writers
-	outLWIR.open(NAMELWIR, CV_FOURCC('X','2','6','4'), FPS, SLWIR, false);
-	if (!outLWIR.isOpened())
-    {
-        throw runtime_error("Could not open the output video for write: LWIR");
-        exit(1);
-    }
-
-	outRGB.open(NAMERGB, CV_FOURCC('X','2','6','4'), FPS, SRGB, true);
-	if (!outRGB.isOpened())
-    {
-        throw runtime_error("Could not open the output video for write: RGB");
-        exit(1);
-    }
+	/* Start threading for sync input */
+	cap.read(buffer);
+	t = thread(task, &cap);
     
+    namedWindow("RGB",  WINDOW_NORMAL);
     namedWindow("LWIR",  WINDOW_NORMAL);
-	namedWindow("RGB",  WINDOW_NORMAL);
+	
 
-	cout << "Start recording" << endl;
+	cout << "Finished init." << endl << "Start recording." << endl << "--------------------" << endl;
 	while(running) {
-		cap.grab();
-		seek.grab();
-		cap.retrieve(frameRGB);
-		frameLWIR = seek.retrieve();	
+		/* Seek LWIR and Normal camera */
+		mtxCam.lock();
+			if(!(seek.grab())){
+				cout << "no more LWIR img to grab" << endl;
+				return false;
+			}
+			buffer.copyTo(frame[RGB]);
+        mtxCam.unlock();
+		if(!(seek.retrieve(frame[LWIR]))){
+			cout << "no more LWIR img to retrieve" << endl;
+			return false;
+		}
 		
-		frameLWIR.convertTo(frameLWIR, CV_8UC1, 1.0/32.0);
-		equalizeHist(frameLWIR, frameLWIR); 
+		frame[LWIR].convertTo(frame[LWIR], CV_8UC1, 1.0/110.0);
+		equalizeHist(frame[LWIR], frame[LWIR]); 
 		
-		cout << nimg++ << endl;
 
-		outRGB << frameRGB;
-		outLWIR << frameLWIR;
+		/* Look at that art */
+		imshow("LWIR", frame[LWIR]);
+		imshow("RGB", frame[RGB]);
 		
-		imshow("LWIR", frameLWIR);
-		imshow("RGB", frameRGB);
+		/* Save the art for future generations */
+		char name [32];
+		sprintf(name, "out/rgb/%05d.png", nimg);
+		imwrite(name, frame[RGB]);
+		sprintf(name, "out/lwir/%05d.png", nimg);
+		imwrite(name, frame[LWIR]);
+		nimg ++;
 		
-		frameLWIR.release();
-		frameRGB.release();
+		/* Release the kraken 
+		 * ... or just the memory, that's also fine */
+		for(int i=0; i<2; i++){
+			frame[i].release();
+		}
 		
-		//waitKey(124);
-		waitKey(1000);//1sec wait => recording calib videos => good frames not bunched together
+		/* Let's wait on tht slow seek */
+		char c = char(waitKey(124));
+		if(c == 'q'){
+			running = false;
+		}
 	}
-	cout << "Finished recording" << endl;
-}
-
-void quitProgram(int sig)
-{
 	running = false;
+	t.join();
+	seek.release();
+	cap.release();
+	cout << "Finished recording" << endl;
+	
+	return 0;
 }
 
 static void help(char* name)
 {
+	/* Let's explain it for the not so smart people */
     cout
         << "------------------------------------------------------------------------------" << endl
         << "This program records the webcam and LWIR."                                      << endl
         << "Usage:"                                                                         << endl
-        << "./" << name << " FPS [webcam nr]"                                               << endl
+        << "./" << name << " webcamnr gradient.png"                                               << endl
         << "------------------------------------------------------------------------------" << endl
         << endl;
+}
+
+void task(VideoCapture *cap)
+{
+	/* Thread to keep the linux video buffer empty */
+	while(running){
+		cap->grab();
+		if(mtxCam.try_lock()){
+			cap->retrieve(buffer);
+			mtxCam.unlock();
+		}
+		usleep(100);
+	}
 }
